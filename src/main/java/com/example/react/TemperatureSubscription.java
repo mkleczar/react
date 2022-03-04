4ba7bda178faf9d4b0387cbbef825e0f1f6c876f
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-03-04 20:37:48 +0100

    subscription impl

---
+++ src/main/java/com/example/react/TemperatureSubscription.java
@@ -0,0 +1,16 @@
+package com.example.react;
+
+import java.util.concurrent.Flow;
+
+public class TemperatureSubscription implements Flow.Subscription {
+
+    @Override
+    public void request(long n) {
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
