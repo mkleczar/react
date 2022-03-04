7f468e38c09f9930d16dbbe47b6274fc06460906
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-03-04 22:19:44 +0100

    subscriber implementation

--- src/main/java/com/example/react/TemperatureSubscription.java
+++ src/main/java/com/example/react/TemperatureSubscription.java
@@ -1,8 +1,17 @@
 package com.example.react;
 
-import java.util.concurrent.Flow;
+import static java.util.concurrent.Flow.Subscriber;
+import static java.util.concurrent.Flow.Subscription;
 
-public class TemperatureSubscription implements Flow.Subscription {
+public class TemperatureSubscription implements Subscription {
+
+    private final Subscriber<? super TemperatureInfo>  subscriber;
+    private final String city;
+
+    public TemperatureSubscription(Subscriber<? super TemperatureInfo> subscriber, String city) {
+        this.subscriber = subscriber;
+        this.city = city;
+    }
 
     @Override
     public void request(long n) {
