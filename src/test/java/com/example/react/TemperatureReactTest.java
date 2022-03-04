2dd158b1678929c7f3e8a7ffab87d25413b91e14
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-03-04 22:58:11 +0100

    main - subscriber

--- src/test/java/com/example/react/TemperatureReactTest.java
+++ src/test/java/com/example/react/TemperatureReactTest.java
@@ -2,6 +2,7 @@ package com.example.react;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Publisher;
 
 public class TemperatureReactTest {
@@ -9,5 +10,7 @@ public class TemperatureReactTest {
     @Test
     public void reactTest() {
         Publisher<TemperatureInfo> publisher = new TemperaturePublisher("London");
+
+        Subscriber<TemperatureInfo> subscriber = new TemperatureSubscriber();
     }
 }
