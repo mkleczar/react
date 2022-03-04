2129657c15e96e463fca6dc9a9fddb47f393390d
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-03-04 22:54:21 +0100

    publisher implementation - subscribe

--- src/main/java/com/example/react/TemperaturePublisher.java
+++ src/main/java/com/example/react/TemperaturePublisher.java
@@ -5,7 +5,15 @@ import java.util.concurrent.Flow.Publisher;
 
 public class TemperaturePublisher implements Publisher<TemperatureInfo> {
 
+    private final TemperatureHub temperatureHub = new TemperatureHub();
+    private final String city;
+
+    public TemperaturePublisher(String city) {
+        this.city = city;
+    }
+
     @Override
     public void subscribe(Subscriber<? super TemperatureInfo> subscriber) {
+        subscriber.onSubscribe(new TemperatureSubscription(temperatureHub, subscriber, city));
     }
 }
