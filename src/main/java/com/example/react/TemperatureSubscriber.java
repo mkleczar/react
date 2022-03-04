abbab99e33703f3fa8185e3fcdb71a24e7ab557b
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-03-04 22:34:59 +0100

    subscriber implementation - stub

---
+++ src/main/java/com/example/react/TemperatureSubscriber.java
@@ -0,0 +1,26 @@
+package com.example.react;
+
+import java.util.concurrent.Flow;
+
+public class TemperatureSubscriber implements Flow.Subscriber<TemperatureInfo> {
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+
+    }
+
+    @Override
+    public void onNext(TemperatureInfo item) {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+}
