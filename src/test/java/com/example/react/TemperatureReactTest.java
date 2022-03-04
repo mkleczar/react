292aac1d3925debb22148ec6f8cfd6d95115405c
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-03-04 22:56:25 +0100

    main - publisher

--- src/test/java/com/example/react/TemperatureReactTest.java
+++ src/test/java/com/example/react/TemperatureReactTest.java
@@ -2,10 +2,12 @@ package com.example.react;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.Flow.Publisher;
+
 public class TemperatureReactTest {
 
     @Test
     public void reactTest() {
-
+        Publisher<TemperatureInfo> publisher = new TemperaturePublisher("London");
     }
 }
